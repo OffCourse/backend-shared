8b4126d17ba2f85d8c74745a2dd95f40fcefb385
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-11-15 21:39:12 +0100

    wip: queries implemented

--- src/backend_shared/es/to_query.cljs
+++ src/backend_shared/es/to_query.cljs
@@ -1,11 +1,15 @@
 (ns backend-shared.es.to-query
   (:require [shared.protocols.specced :as sp]
-            [cuerdas.core :as str]))
+            [cuerdas.core :as str]
+            [shared.protocols.loggable :as log]))
 
 (defn query [subquery]
   {:query {:bool subquery}})
 
 (defmulti to-query sp/resolve)
+
+(defmethod to-query :resource [{:keys [resource-url]}]
+  (query {:must [{:match {:resource-url resource-url}}]}))
 
 (defmethod to-query :course [{:keys [course-slug curator]}]
   (query {:must [{:match {:goal (str/humanize course-slug)}}
